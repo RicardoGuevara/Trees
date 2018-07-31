2d6754629e37014493470773e03f8bbd24fc4952
RicardoGuevara <ricrdogp@protonmail.com>
2018-07-31 10:46:30 -0500

    re_orders

--- Tree/src/nodes/BiNode.java
+++ Tree/src/nodes/BiNode.java
@@ -5,6 +5,8 @@
  */
 package nodes;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ESTU-5K
@@ -20,6 +22,24 @@ public class BiNode<C extends Comparable> extends Node{
         this.root = root;
         this.init();
     }
+    
+    public BiNode(BiNode root, int content) {
+        super((Integer)content);
+        this.root = root;
+        this.init();
+    }
+    
+    public BiNode(BiNode root, float content) {
+        super((Float)content);
+        this.root = root;
+        this.init();
+    }
+
+    public BiNode(BiNode root, byte content) {
+        super((Byte)content);
+        this.root = root;
+        this.init();
+    }
 
     public BiNode(C content) {
         super(content);
@@ -30,6 +50,9 @@ public class BiNode<C extends Comparable> extends Node{
     
 //METHODS_______________________________________________________________________
     
+    /**
+     * verifica e inicia parámetros en el nodo
+     */
     private void init()
     {
         try 
@@ -43,20 +66,166 @@ public class BiNode<C extends Comparable> extends Node{
         }
     }
     
+    /**
+     * añade un elemento al arbol
+     * @param ob 
+     */
+    public void add(C ob)
+    {
+        new BiNode(this,ob);
+    }
+    
+    public void add(int ob)
+    {
+        new BiNode(this,ob);
+    }
+    
+    public void add(float ob)
+    {
+        new BiNode(this,ob);
+    }
+    
+    public void add(byte ob)
+    {
+        new BiNode(this,ob);
+    }
+    
+    public void add(BiNode node)
+    {
+        this.locate(node);
+    }
+    
+    /**
+     * ubica de forma ordenada un elemento en el arbol
+     * @param node 
+     */
     private void locate(BiNode node)
     {
         switch((this.getContent()).compareTo(node.getContent()))
         {
             case -1:
-                //recursivo izquierda
+                if (this.getLeft()==null) 
+                {
+                    node.setLeft(node);
+                }
+                else
+                {
+                    this.getLeft().locate(node);
+                }
                 break;
             case 1:
-                //recursivo derecha
+                if (this.getRight()==null) 
+                {
+                    node.setRight(node);
+                }
+                else
+                {
+                    this.getRight().locate(node);
+                }
                 break;
             default:
-                //elemento descartado
+                //case 0, elemento descartado
                 break;
         }
     }
+    
+    /**
+     * imprime los elementos del árbol en preorden
+     */
+    public void preOrden()
+    {
+        System.out.println(this.getContent());
+        if (this.getLeft()!=null) 
+        {
+            this.getLeft().preOrden();
+        }
+        if (this.getRight()!=null) 
+        {
+            this.getLeft().preOrden();
+        }
+    }
+    
+    /**
+     * imprime los elementos del árbol en inorden
+     */
+    public void inOrden()
+    {
+        if (this.getLeft()!=null) 
+        {
+            this.getLeft().inOrden();
+        }
+        System.out.println(this.getContent());
+        
+        if (this.getRight()!=null) 
+        {
+            this.getLeft().inOrden();
+        }
+    }
+    
+    /**
+     * imprime los elementos del árbol en posorden
+     */
+    public void posOrden()
+    {
+        if (this.getLeft()!=null) 
+        {
+            this.getLeft().posOrden();
+        }
+        if (this.getRight()!=null) 
+        {
+            this.getLeft().posOrden();
+        }
+        System.out.println(this.getContent());
+    }
+    
+    /**
+     * imprime  los elementos del árbol por  niveles
+     */
+    public void perLevels()
+    {
+        ArrayList<BiNode> array = new ArrayList<>();
+        array.add(this);
+        
+        while(!array.isEmpty())
+        {
+            System.out.println(array.get(0));
+            if (array.get(0).getLeft()!=null) 
+            {
+                array.add(array.get(0).getLeft());
+            }
+            if (array.get(0).getRight()!=null) 
+            {
+                array.add(array.get(0).getRight());
+            }
+            array.remove(0);
+        }
+    }    
+    
+ //GETTERS & SETTERS____________________________________________________________   
+
+    public BiNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BiNode left) {
+        this.left = left;
+    }
+
+    public BiNode getRight() {
+        return right;
+    }
+
+    public void setRight(BiNode right) {
+        this.right = right;
+    }
+
+    public BiNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(BiNode root) {
+        this.root = root;
+    }
+    
     
 }
--- Tree/src/tree/Tree.java
+++ Tree/src/tree/Tree.java
@@ -19,28 +19,8 @@ public class Tree {
      */
     public static void main(String[] args) 
     {    
-        //root node
-        BiNode<String> node = new BiNode("root");
         
-        BiNode<String> node11 = new BiNode("izq",node);
-        BiNode<String> node12 = new BiNode("der",node);
         
-        BiNode<String> node21 = new BiNode("izqizq",node11);
-        BiNode<String> node22 = new BiNode("izqder",node11);
-        
-        BiNode<String> nod23 = new BiNode("derizq",node12);
-        BiNode<String> nod24 = new BiNode("derder",node12);
-        
-        System.out.println("altura: " +node.getHeigth()+"\n");
-        System.out.println("inorden_____________________________________________");
-        node.inOrden();
-        System.out.println("preorden_____________________________________________");
-        node.preOrden();
-        System.out.println("posorden_____________________________________________");
-        node.posOrden();
-        System.out.println("perlevels_____________________________________________");
-        node.perLevels();
-
     }
     
 }
